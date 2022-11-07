6060575a4d3c1325c1e8e60ea94558f7fc37ae87
karthick0112 <116705837+karthick0112@users.noreply.github.com>
2022-11-07 11:55:18 +0530

    java

---
+++ Fact.java
@@ -0,0 +1,14 @@
+package factorial;
+
+public class Fact {
+
+	public static void main(String[] args) {
+		int num=5,fact=1;
+		// TODO Auto-generated method stub
+		 for(int i=1;i<=num;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+num+" is: "+fact);    
+	}
+
+}
---
+++ array.java
@@ -0,0 +1,13 @@
+ class array 
+
+    {
+
+
+         public static void main(String[] args){
+
+                int[] arr = new int[5];
+
+arr = new int[6];
+        }
+
+	}
